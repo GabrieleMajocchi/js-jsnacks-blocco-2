d6d03fa68b66c465e7be01c78e76ece75b7f4e4f
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-19 17:34:55 +0200

    guess the number snacks-2

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,47 @@
 
-const UserNumbers = [];
+// ---Primo Snack---
 
-console.log(UserNumbers);
+// const UserNumbers = [];
 
-let sum = 0;
+// let sum = 0;
+// let i = 0;
+
+
+// while(sum < 50){
+    
+//     UserNumbers.push(parseInt(prompt()));
+//     sum = sum + UserNumbers[i];
+//     i++;
+
+// }
+
+
+let rightNumber = Math.floor(Math.random() * 100) + 1;
+
+console.log(rightNumber)
+
+const Sendit = document.querySelector('button');
+
 let i = 0;
 
-while(sum < 50){
-    
-    UserNumbers.push(parseInt(prompt()));
-    sum = sum + UserNumbers[i];
-    i++;
+win = false
 
-}
+
+    sendit.addEventListener('click', 
+        function(){
+            if(win == false){
+            i++;
+
+            let number = document.getElementById('number').value;
+
+            number = parseInt(number);
+
+            if(number === rightNumber){
+                document.getElementById('result').innerHTML = 'Complimenti hai azzeccato il numero! <br> Con un totale di:'+i+' tentativi';
+                win = true
+            }else{
+                document.getElementById('result').innerHTML = 'Numero errato, sei al tentativo numero: '+i;
+            }
+        }
+        }
+    )
