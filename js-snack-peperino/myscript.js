12ee14a103cd8393d016635852fccc3b86afa44a
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-29 12:06:23 +0200

    created myscript that create a peperonis list with random info of them

---
+++ js-snack-peperino/myscript.js
@@ -0,0 +1,32 @@
+
+const peperonis = []
+const pepType = ['red', 'yellow', 'green'];
+
+for (i=0; i<10; i++){
+peperonis.push(randomPeperoni(pepType));
+}
+
+console.log(peperonis)
+// ---Generate random stats---
+
+
+function randomPeperoni (listoftype){
+    const peperone = {};
+    peperone.type = randomtype(listoftype);
+    peperone.weight = randomNum(1, 20);
+    peperone.lenght = randomNum(10, 40);
+    return peperone;
+}
+
+
+function randomtype (listoftype){
+    const number = Math.floor(Math.random() * 2) + 0
+    const type = listoftype[number]
+    return type;
+}
+
+function randomNum (min, max){
+    const number = Math.floor(Math.random() * max) + min
+    return number;
+}
+
