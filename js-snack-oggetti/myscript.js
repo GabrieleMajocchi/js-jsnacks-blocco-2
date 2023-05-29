8b1aab6426e286bb7633ba6690e2f940b7f66a53
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-29 11:20:33 +0200

    added random stats writer

--- js-snack-oggetti/myscript.js
+++ js-snack-oggetti/myscript.js
@@ -10,4 +10,39 @@ const basketPlayer = {
     Tiri: 8,
 }
 
+// ---Generate random stats---
+
+basketPlayer['Codice giocatore'] = randStr(3)+randNum(3);
+basketPlayer['Media punti fatti per partita'] = randomNum(0, 50);
+basketPlayer['Percentuale di successo per tiri da 3 punti'] = randomNum(0, 100);
+basketPlayer.Stoppate = randomNum(0, 30);
+basketPlayer.Tiri = randomNum(20, 80);
+
+
 console.log(basketPlayer);
+
+function randomNum (min, max){
+    const number = Math.floor(Math.random() * max) + min
+    return number;
+}
+
+function randStr(lenght) {
+    const list = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+    var res = "";
+    for(var i = 0; i < lenght; i++) {
+        var rnd = Math.floor(Math.random() * list.length);
+        res = res + list.charAt(rnd);
+    }
+    return res;
+}
+
+
+function randNum(lenght) {
+    const list = "123456789";
+    var res = "";
+    for(var i = 0; i < lenght; i++) {
+        var rnd = Math.floor(Math.random() * list.length);
+        res = res + list.charAt(rnd);
+    }
+    return res;
+}
