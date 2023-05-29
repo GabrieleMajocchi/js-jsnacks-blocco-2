1803d4d48e4bf898bbf6036dacdb8b44e48a8043
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-29 11:10:06 +0200

    Created myscript with basket player stats

---
+++ js-snack-oggetti/myscript.js
@@ -0,0 +1,13 @@
+
+const basketPlayer = {
+    'Codice giocatore': 21,
+    Nome: 'Michele',
+    Cognome: 'Giordano',
+    Età: 25,
+    'Media punti fatti per partita': 15,
+    'Percentuale di successo per tiri da 3 punti': 5,
+    Stoppate: 3, 
+    Tiri: 8,
+}
+
+console.log(basketPlayer);
